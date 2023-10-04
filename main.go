521fd5515bb430f7c27436df28466764b542d052
narges <nargesbyt123@gmail.com>
2023-10-04 19:57:03 +0330

    add cache

--- main.go
+++ main.go
@@ -1,30 +1,65 @@
 package main
 
 import (
-	dnsresolve "dnsResolver/dns"
+	"dnsResolver/dns"
+	"dnsResolver/dns/cache"
+	"errors"
 	"fmt"
-	"log"
 	"net"
+	"os"
+
+	//"git.mills.io/prologic/bitcask"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
 )
 
+func checkCacheFolder() {
+	if _, err := os.Stat(".cache"); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(".cache", os.ModePerm)
+		if err != nil {
+			log.Err(err).Msg("unable to make directory")
+		}
+	}
+}
+
 func main() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	log.Logger = zerolog.New(os.Stderr).
+		Level(zerolog.InfoLevel).
+		With().
+		Timestamp().
+		Caller().
+		Logger()
+
 	fmt.Println("starting a dns server ...")
+
 	packetConnection, err := net.ListenPacket("udp", ":8080")
 	if err != nil {
-		log.Fatal(err)
-		//panic(err)
+		log.Fatal().Err(err).Msg("unable to make connection")
 
 	}
 	defer packetConnection.Close()
+
+	r := dns.Resolver{
+		Cache: &cache.InMemoryCache{make(map[string]interface{})},
+	}
+
 	for {
 		buf := make([]byte, 512)
+		//ReadFrom read a packet from connection and copy the payload into buf
 		bytesRead, addr, err := packetConnection.ReadFrom(buf)
 		if err != nil {
 			//fmt.Printf("read error from%s:%s", addr.String(), err)
-			log.Fatal(err)
+			log.Fatal().Err(err).Msg("unable to read packet")
 			continue
 		}
-		 dnsresolve.HandlePacket(packetConnection, addr, buf[:bytesRead])
+		packet := dns.Packet{
+			Conn: packetConnection,
+			Address: addr,
+			Body: buf[:bytesRead],
+		}
+		r.HandlePacket(packet)
 
 	}
 
